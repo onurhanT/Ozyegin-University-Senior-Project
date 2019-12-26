dc526de4ab89a6729d2585b90cae237728217239
Onurhan Turfanda <onurhan.turfanda@ozu.edu.tr>
2019-12-26 14:20:44 +0300

    Update 0.1.1

---
+++ Senior-Project/Assets/Scripts/Piano/Piano_Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piano_Controller : MonoBehaviour
+{
+    
+    
+    private AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sound.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        sound.Stop();
+    }
+}
